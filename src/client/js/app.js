8dbbecf8a6603737d8757d91a75056864b6cb57e
Leigh Bitossi <leeroy77@gmailcom>
2021-04-22 21:00:37 +0900

    Style changes for different sizes

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -188,7 +188,7 @@ const updateUI = async () => {
         document.getElementById('min').innerHTML = `Expected low: ${allData[allData.length - 2].arrivalLow}°C`;
         document.getElementById('temp').innerHTML = `Expected high: ${allData[allData.length - 2].arrivalHigh}°C`;
         document.getElementById('condition').innerHTML = `Weather now in ${allData[allData.length - 4].cityname}`;
-        document.getElementById('arrival-condition').innerHTML = `Weather on arrival: ${allData[allData.length - 2].arrival}`;
+        document.getElementById('arrival-condition').innerHTML = `Arrival: ${allData[allData.length - 2].arrival}`;
         document.getElementById('country').innerHTML = `Country: ${allData[allData.length - 4].country}`;
         document.getElementById('theCity').innerHTML = `City name: ${allData[allData.length - 4].cityname}`;
         document.getElementById('cityImage').innerHTML = `<img src="${allData[allData.length - 3].image}" id = "city-pic"/>`;
